8ce26858c09b6593f144e926f3d690de8e707355
JoMULLOA <jose.manriquez2101@alumnos.ubiobio.cl>
2025-11-20 17:51:09 -0300

    Subir actividad vacia
    Se sube una actividad vacia para empezar el desarrollo del proyecto

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+}
+}
+    }
+        mavenCentral()
+        google()
+    repositories {
+buildscript {
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("com.android.application") version "8.0.0" apply false
+
